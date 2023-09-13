0a575d7b4bdd934f02c81c3dad32b0f20c10dc05
khadija <ameksakhadija026@gmail.com>
2023-09-13 15:05:36 +0100

    chalange4

---
+++ chalange4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A,B,C,D;
+    float P;
+    printf("voullez entrer les valeures de A et B et C et D");
+    scanf("%d",&A);
+    scanf("%d",&B);
+    scanf("%d",&C);
+    scanf("%d",&D);
+    P=A+B+B+D;
+    printf("la somme de A et B et C et D EST:%f",P);
+    printf("la moyenne de A et B et C et D EST:%f",P/4);
+    return 0;
+}
