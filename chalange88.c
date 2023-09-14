7c40d5b8ec192e5b11defec9ae2db8f7329a78a5
khadija <ameksakhadija026@gmail.com>
2023-09-14 09:17:50 +0100

    chalange8

---
+++ chalange88.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number,quotient,octal_num,hexa_num;
+
+    printf("enter a number/n");
+    scanf("%d",& number);
+    printf("octal number of a giving number=%o /n",number);
+    printf("hexadecimal number of a giving number=%x /n",number);
+    return 0;
+}
