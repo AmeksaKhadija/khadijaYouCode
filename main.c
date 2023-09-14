d8d82166d59656f6ee921173c934d56adcc9e943
khadija <ameksakhadija026@gmail.com>
2023-09-14 09:55:24 +0100

    chalange5

--- main.c
+++ main.c
@@ -3,12 +3,17 @@
 
 int main()
 {
-   float C,F;
-    printf("entrer la temperature en Fahrenheit");
-    scanf("%f",&F);
-    C = (F-32)/1.8;
-    printf("la temperature en  Celsius est:%f",C);
-
+    float distance;
+    int x1,x2,y1,y2;
+    printf("entrer la valeur de x1");
+    scanf("%d",&x1);
+    printf("entrer la valeur de x2");
+    scanf("%d",&x2);
+    printf("entrer la valeur de y1");
+    scanf("%d",&y1);
+    printf("entrer la valeur de y2");
+    scanf("%d",&y2);
+    distance=sqrt (pow((x2-x1),2)+pow((y2-y1),2));
+    printf("la distance entre deux points est %f",distance);
     return 0;
 }
-
