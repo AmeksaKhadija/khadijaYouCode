d95f2fa1bd7e938ee6b8cdb969b1c69752a8a92b
khadija <ameksakhadija026@gmail.com>
2023-09-14 10:46:58 +0100

    condition2

---
+++ COND2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char voyelle;
+    printf("rntrer la valeur du voyelle");
+    scanf("%c",&voyelle);
+    switch(voyelle){
+    case 'a':
+    printf("la caractere est un voyelle");
+    case 'A':
+    printf("la caractere est un voyelle");
+    case 'o':
+    printf("la caractere est un voyelle");
+    case 'O':
+    printf("la caractere est un voyelle");
+    case 'i':
+    printf("la caractere est un voyelle");
+    case 'I':
+    printf("la caractere est un voyelle");
+    case 'u':
+    printf("la caractere est un voyelle");
+    case 'U':
+    printf("la caractere est un voyelle");
+    case 'e':
+    printf("la caractere est un voyelle");
+    case 'E':
+    printf("la caractere est un voyelle");
+    }
+    return 0;
+}
