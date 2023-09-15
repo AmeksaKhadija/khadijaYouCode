5ccb4edc5d4fc001b200521176928afa324c8e9c
khadija <ameksakhadija026@gmail.com>
2023-09-15 10:28:04 +0100

    condition9

--- main.c
+++ main.c
@@ -3,17 +3,18 @@
 
 int main()
 {
-    float distance;
-    int x1,x2,y1,y2;
-    printf("entrer la valeur de x1");
-    scanf("%d",&x1);
-    printf("entrer la valeur de x2");
-    scanf("%d",&x2);
-    printf("entrer la valeur de y1");
-    scanf("%d",&y1);
-    printf("entrer la valeur de y2");
-    scanf("%d",&y2);
-    distance=sqrt (pow((x2-x1),2)+pow((y2-y1),2));
-    printf("la distance entre deux points est %f",distance);
+    char cara;
+    printf("entrer la valeur du cara");
+    scanf("%c",&cara);
+    if (cara >65 && cara<90){
+        printf("le caractere %c est un alphabet majuscule", cara);
+    }
+    else if (cara >97 && cara<133){
+        printf("le caractere est miniscule ");
+    }
+    else {
+        printf("le caractere pas un alphabet");
+    }
     return 0;
+
 }
