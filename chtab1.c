52ef123c33d8e954c44d79655d51e66a8e536ba6
khadija <ameksakhadija026@gmail.com>
2023-09-18 09:50:04 +0100

    tableau1

---
+++ chtab1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int T[10];
+   int i;
+   for (i=0;i<10;i++){
+      T[i]=i+1;
+      }
+    for (i=0;i<10;i++){
+      printf("%d ,",T[i]);
+    }
+
+    return 0;
+}
