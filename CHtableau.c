5b1aaf775298b65e0007849ec0718d89880dd178
khadija <ameksakhadija026@gmail.com>
2023-09-19 16:43:10 +0100

    chaine

---
+++ CHtableau.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char T[30];
+    char i;
+    int compteur;
+     printf("entrer votre nom :");
+     scanf("%s",&T[i]);
+     compteur=0;
+   while(T[compteur]!='\0')
+        compteur++;
+          printf("la taille de votre caractaire est : %d",compteur);
+    return 0;
+}
